05c567027db3d7d2eb35b03a5f3d229ad8556ef4
JonahLargen <jhlargen2@gmail.com>
2025-01-19 20:51:23 -0500

    Draw Line Between Cells

--- src/cell.py
+++ src/cell.py
@@ -28,3 +28,6 @@ class Cell:
             self.__win.draw_line(Line(Point(x1, y1), Point(x2, y1)), "black")
         if self.has_bottom_wall:
             self.__win.draw_line(Line(Point(x1, y2), Point(x2, y2)), "black")
+            
+    def draw_move(self, to_cell, undo=False):
+        self.__win.draw_line(Line(Point((self.__x1 + self.__x2) / 2, (self.__y1 + self.__y2) / 2), Point((to_cell.__x1 + to_cell.__x2) / 2, (to_cell.__y1 + to_cell.__y2) / 2)), "gray" if not undo else "red")
--- src/main.py
+++ src/main.py
@@ -9,6 +9,14 @@ def main():
     cell = Cell(win)
     cell.has_bottom_wall = False
     cell.draw(200, 300, 300, 400)
+    cell2 = Cell(win)
+    cell2.has_right_wall = False
+    cell2.draw(300, 300, 400, 400)
+    cell.draw_move(cell2)
+    cell3 = Cell(win)
+    cell3.has_top_wall = False
+    cell3.draw(400, 400, 500, 500)
+    cell2.draw_move(cell3, True)
     win.wait_for_close()
 
 main()
