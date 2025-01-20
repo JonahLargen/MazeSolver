52f11c1bde13759eb1dd48037c834b177a4c9322
JonahLargen <jhlargen2@gmail.com>
2025-01-19 20:21:36 -0500

    Draw Line

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+from point import Point
+from line import Line
 from window import Window
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(50, 50), Point(100, 100)), "red")
     win.wait_for_close()
 
 main()
