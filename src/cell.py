54d400bd736ed2be91175a75ea7ad884285e8ec2
JonahLargen <jhlargen2@gmail.com>
2025-01-22 19:53:39 -0500

    Reset Visited

--- src/cell.py
+++ src/cell.py
@@ -12,7 +12,7 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self._visited = False
+        self.visited = False
         
     def draw(self, x1, y1, x2, y2):
         if (self.__win is None):
--- src/maze.py
+++ src/maze.py
@@ -19,6 +19,7 @@ class Maze:
         self._create_cells()
         self._break_entrance_and_exit()
         self._break_walls_r(0, 0)
+        self._reset_cells_visited()
 
     def _create_cells(self):
         self._cells = []
@@ -47,24 +48,24 @@ class Maze:
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
 
     def _break_walls_r(self, i, j):
-        self._cells[i][j]._visited = True
+        self._cells[i][j].visited = True
         while True:
             l = []
             if i > 0:
                 left = self._cells[i - 1][j]
-                if not left._visited:
+                if not left.visited:
                     l.append((i - 1, j, 'left'))
             if i < self._num_cols - 1:
                 right = self._cells[i + 1][j]
-                if not right._visited:
+                if not right.visited:
                     l.append((i + 1, j, 'right'))
             if j > 0:
                 top = self._cells[i][j - 1]
-                if not top._visited:
+                if not top.visited:
                     l.append((i, j - 1, 'top'))
             if j < self._num_rows - 1:
                 bottom = self._cells[i][j + 1]
-                if not bottom._visited:
+                if not bottom.visited:
                     l.append((i, j + 1, 'bottom'))
             if len(l) == 0:
                 self._draw_cell(i, j)
@@ -88,3 +89,8 @@ class Maze:
                 self._cells[i][j].has_bottom_wall = False
                 self._cells[i2][j2].has_top_wall = False
             self._break_walls_r(i2, j2)
+            
+    def _reset_cells_visited(self):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False
