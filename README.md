8ca6e638e178c4902744c3db9bd2185f1b8df74c
JonahLargen <jhlargen2@gmail.com>
2025-02-05 18:37:52 -0500

    Add Paramaters

--- README.md
+++ README.md
@@ -7,10 +7,6 @@ WIP
 
 A python application that builds a maze from scratch using parameters and then solves it. The maze solving is done using [tkinter](https://docs.python.org/3/library/tkinter.html) to provide a visual UI so you can watch the program solve the maze in real time. The maze is constructed and solved using a Depth First Search (DFS) algorithm. This tool does not intend to be the most optimal way to build or solve a maze but rather showcase DFS being used in a real world application.
 
-## How It Works
-
-
-
 > [!NOTE]
 > If you look at the source code, you may be shocked to see a very high recursion limit being used. This number may need to be increased further for very large mazes. Do not be alarmed, as the DFS recursion that is used to both build and solve the maze runs in O(n) time. Because cells are only visited once, you always selected one of four neighbors, and all work such as animation and drawing is constant, the function runs in linear time.
 
--- src/main.py
+++ src/main.py
@@ -1,19 +1,32 @@
 from window import Window
 from maze import Maze
 import sys
+import argparse
 
 sys.setrecursionlimit(100_000)
 
 def main():
-    width = 1200
-    height = 900
-    margin = 100
-    cell_size = 50
-    delay = 0.01
+    parser = argparse.ArgumentParser(description="Process some command line arguments.")
+    
+    parser.add_argument('--width', type=int, default=800, help='Width in pixels')
+    parser.add_argument('--height', type=int, default=600, help='Height in pixels')
+    parser.add_argument('--margin', type=int, default=50, help='Margin in pixels')
+    parser.add_argument('--size', type=int, default=100, help='Cell size in pixels')
+    parser.add_argument('--delay', type=float, default=0.05, help='Delay in seconds')
+    parser.add_argument('--seed', type=int, default=None, help='Seed used in random.seed(...)')
+    
+    args = parser.parse_args()
+    
+    width = args.width
+    height = args.height
+    margin = args.margin
+    cell_size = args.size
+    delay =args.delay
+    seed = args.seed
     win = Window(width, height)
     num_rows = (height - margin * 2) // cell_size
     num_cols = (width - margin * 2) // cell_size
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size, cell_size, delay, win, 0)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size, cell_size, delay, win, seed)
     maze.solve()
     win.wait_for_close()
 
